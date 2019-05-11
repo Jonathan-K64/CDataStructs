ab40f1e0d60cbb932160d94db9079d7cfbd6f8e8
Jonathan-K64 <47532252+Jonathan-K64@users.noreply.github.com>
2019-05-11 14:11:10 -0400

    Update sll.h

--- sll.h
+++ sll.h
@@ -1,6 +1,6 @@
 // Jonathan Kraft 05.09.19
 //
-// sll.h
+// LinkedList.h
 //
 // Header File for Basic Singly-Linked List Functions
 
@@ -8,53 +8,53 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef struct linkedListNode *Node;
+typedef struct node *List;
 
 // Create a new null list on a pointer
-void createNew (Node *lptr);
+void createNew (List *lptr);
 
 // Add a new value to the front of the list
-void addToF (Node *lptr, int x);
+void addToF (List *lptr, int x);
 
 // Add a new value to a specified location in the list (non-negative integer
 // only).  Note: the head is enumerated loc = 0, the second element would be 
 // enumerated loc = 1, etc.  If the list is long enough to accomodate this, 
 // the function will return true; otherwise, the value will be added to the
 // end and false is returned
-bool addToLoc (Node *lptr, int x, int loc);
+bool addToLoc (List *lptr, int x, int loc);
 
 // Remove the first instance of a value from the list and return true, 
 // or return false if the value is not present anywhere in the list
-bool deleteX (Node *lptr, int x);
+bool deleteX (List *lptr, int x);
 
 // Remove all instances of a value from the list and return how many nodes
 // were removed
-int deleteAllX (Node *lptr, int x);
+int deleteAllX (List *lptr, int x);
 
 // Iterate through the list and determine whether a value is present
-bool findX (Node l, int x);
+bool findX (List l, int x);
 
 // Iterate through a list and determine what position in the list the first 
 // instance of a value is located.  Note: the head is enumerated loc = 0, 
 // the second element would be enumerated loc = 1, etc.  If the value is not 
 // present, -1 is returned
-int findXLoc (Node l, int x);
+int findXLoc (List l, int x);
 
 // Determine the number of nodes there are in the list
-int listSize (Node l);
+int listSize (List l);
 
 // Print every element's data from the list in order
-void printAll (Node l);
+void printAll (List l);
 
 // Print first n elements' data from the list in order
-void printN (Node l, int n);
+void printN (List l, int n);
 
 // Print every elements' data from the list in order.  If x is ever found, stop
 // and return true; otherwise, print the whole list and then return false
-bool printToX (Node l, int x);
+bool printToX (List l, int x);
 
 // Print each element's data from the list in order and then free it from storage
-void dump (Node *lptr);
+void dump (List *lptr);
 
 // Free all storage from a list and make it null
-void freeAll (Node *lptr);
+void freeAll (List *lptr);
