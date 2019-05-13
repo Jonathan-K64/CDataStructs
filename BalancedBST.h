4b5700dec9b41ec37833301fe12de9443835e95d
Jonathan-K64 <47532252+Jonathan-K64@users.noreply.github.com>
2019-05-13 09:54:00 -0400

    Update BalancedBST.h

--- BalancedBST.h
+++ BalancedBST.h
@@ -1,4 +1,4 @@
-// Jonathan Kraft 04.29.19
+// Jonathan Kraft 05.12.19
 //
 // BalancedBST.h
 //
@@ -14,12 +14,31 @@ typedef struct node *Tree;
 // Create an empty tree and return it
 Tree createNew (void);
 
+// Checks to see if the tree is empty, returns true if so and false otherwise
+bool isEmpty (Tree t);
+
+// Checks to see if the value x is present in the tree, returns true if so
+// and false otherwise
+bool containsX (Tree t, int x);
+
 // Insert the value x into the tree and then return the larger tree
 Tree insertX (Tree t, int x);
 
 // Delete the value x from tree if it is present, freeing its storage and return
 // modified tree
 Tree deleteX (Tree t, int x);
+
+// Delete the root of the tree and enter its data value into the iptr
+// address
+Tree deleteRoot (Tree t, int *iptr);
+
+// Delete the maximum value within the tree and enter its data value into 
+// the iptr address
+Tree deleteMax (Tree t, int *iptr);
+
+// Delete the minimum value within the tree and enter its data value into 
+// the iptr address
+Tree deleteMin (Tree t, int *iptr);
 
 // Destroy a tree and free all of its storage; return an empty tree
 Tree deleteAll (Tree t);
@@ -32,6 +51,10 @@ void inorder (Tree t);
 
 // Print all values in the tree in postorder with respect to the structure
 void postorder (Tree t);
+
+// Destroy a tree, printing all of its data values in postorder with respect
+// to the structure before freeing each node's storage
+Tree dump (Tree t);
 
 // Return number of nodes in the tree
 int treeSize (Tree t);
