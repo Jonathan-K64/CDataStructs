cbaed158fce30d286fcba3f599749b76b4354cc0
Jonathan-K64 <47532252+Jonathan-K64@users.noreply.github.com>
2019-05-16 17:46:14 -0400

    Update Vector.c

--- Vector.c
+++ Vector.c
@@ -20,17 +20,17 @@ static int getSize (Vector v) {
 
 // Creates a new array, double the size of the old array, and copy over
 // all the values
-static void resize (Vector *vptr) {
+static void resize (Vector v) {
 	// Create a new array twice the size of the old array
-	int oldSize = getSize (*vptr);
+	int oldSize = getSize (v);
 	int *new = malloc (2 * oldSize * sizeof (int));
 	// Write in the new size in the 0th index and copy over the other elements
 	new[0] = 2 * oldSize;
 	for (int i = 1; i < oldSize; i++)
-		new[i] = (*vptr)->array[i];
+		new[i] = v->array[i];
 	// Free the old array and set the pointer to the new array
-	free ((*vptr)->array);
-	(*vptr)->array = new;
+	free (v->array);
+	v->array = new;
 }
 
 // ***** Public Functions *****
@@ -46,24 +46,24 @@ void createNew (Vector *vptr) {
 
 // Append the value x to the end of the vector, expanding the size
 // of the vector if necessary
-void appendX (Vector *vptr, int x) {
+void appendX (Vector v, int x) {
 	// If we're out of space, double the size
-	if ((*vptr)->end == getSize (*vptr))
-		resize (vptr);
-	int end = (*vptr)->end;
-	(*vptr)->array[end] = x;
-	(*vptr)->end++;
+	if (v->end == getSize (v))
+		resize (v);
+	int end = v->end;
+	v->array[end] = x;
+	v->end++;
 }
 
 // Set the specified location to the integer x and return true.  Print
 // a message to stderr and return false if loc is 0, negative, or a 
 // value beyond the end of the vector
-bool setLocValue (Vector *vptr, int loc, int x) {
-	if ((loc <= 0) || (loc >= (*vptr)->end)) {
+bool setLocValue (Vector v, int loc, int x) {
+	if ((loc <= 0) || (loc >= v->end)) {
 		fprintf (stderr, "Invalid location\n");
 		return false;
 	}
-	(*vptr)->array[loc] = x;
+	v->array[loc] = x;
 	return true;
 }
 
@@ -81,15 +81,14 @@ bool peekLoc (Vector v, int loc, int *iptr) {
 
 // Swap the values at the given locations and return true.  Return false
 // if either location is 0, negative, or beyond the end of the vector
-bool swapLocs (Vector *vptr, int loc1, int loc2) {
-	if ((loc1 <= 0) || (loc2 <= 0) || 
-		(loc1 >= (*vptr)->end) || (loc2 >= (*vptr)->end)) {
+bool swapLocs (Vector v, int loc1, int loc2) {
+	if ((loc1 <= 0) || (loc2 <= 0) || (loc1 >= v->end) || (loc2 >= v->end)) {
 		fprintf (stderr, "Invalid location\n");
 		return false;
 	}
-	int temp = (*vptr)->array[loc1];
-	(*vptr)->array[loc1] = (*vptr)->array[loc2];
-	(*vptr)->array[loc2] = temp;
+	int temp = v->array[loc1];
+	v->array[loc1] = v->array[loc2];
+	v->array[loc2] = temp;
 	return true;
 }
 
@@ -113,4 +112,17 @@ void printFrom (Vector v, int start, int end) {
 	// Print the values
 	for (int i = start; i < end; i++)
 		printf ("%d\n", v->array[i]);
+}
+
+// Free all storage occupied by the vector and set the pointer to null
+void deleteAll (Vector *vptr) {
+	free ((*vptr)->array);
+	free (*vptr);
+	*vptr = NULL;
+}
+
+// Print the data in the vector and free all storage
+void dump (Vector *vptr) {
+	printAll (*vptr);
+	deleteAll (vptr);
 }
